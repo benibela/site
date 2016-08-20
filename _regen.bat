d83e31771e45afae195e4b47af994c8888c778b8
benibela <benito@benibela.de>
2016-08-20 15:36:13 +0200

    remove old scripts

--- _regen.bat
+++
@@ -1,4 +0,0 @@
-rem P:\system\java\jdk1.5.0_04\bin\java.exe -cp u:\programs\java\fileparser\publish FileParser process
-
-java.exe -cp u:\programs\java\fileparser\publish;U:\programs\data\jaxmlparser\publish FileParser process
-copy _publish\index_de.html _publish\index.html
